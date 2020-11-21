795b9a8cfa209eb54a1a4960abf7b376b3359866
mohitgehi <mohitgehi23@gmail.com>
2020-11-21 18:11:48 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -23,8 +23,8 @@ const App = (props) => {
         <>
             <Slides currentSlide={slides[currentSlide]}/>
             <button disabled={currentSlide===slides.length-1} onClick={nextSlide} data-testid="button-restart">Next</button>
-            <button disabled={currentSlide===0} onClick={prevSlide} data-testid="button-prev">Previous</button>
-            <button disabled={currentSlide===0} onClick={resetSlide} data-testid="button-restart">Reset</button>
+            <button disabled={currentSlide===0} onClick={prevSlide} data-testid="button-prev">Prev</button>
+            <button disabled={currentSlide===0} onClick={resetSlide} data-testid="button-restart">Restart</button>
         </>
     )
 }
